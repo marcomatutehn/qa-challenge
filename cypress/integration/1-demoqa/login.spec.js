952365003686bfcc9da0bf87516d1b7f1a0ca8ba
Marco <konkerpl>
2022-04-28 17:50:14 -0600

    Login test case

---
+++ cypress/integration/1-demoqa/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Login into demoqa ', () => {
+    before(() => {
+        cy.visit('https://demoqa.com/')
+    })
+
+    it('Login', () => {
+
+    })
+
+
+})
