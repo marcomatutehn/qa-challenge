ef123917c3f6680fc70e2c761b8c7a0db989a4c5
Marco <konkerpl>
2022-04-28 17:54:05 -0600

    Creation of profile

---
+++ cypress/integration/1-demoqa/profile.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Profile in demoqa ', () => {
+    before(() => {
+        cy.visit('https://demoqa.com/')
+    })
+
+    it('Profile', () => {
+
+    })
+
+
+})
